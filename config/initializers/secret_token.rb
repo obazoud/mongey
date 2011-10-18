07bb7407885ce0d2d24a08c68fb040b5663c0ef3
Jonas Lundqvist <lundqvist.j@gmail.com>
2011-10-18 07:07:59 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mongey::Application.config.secret_token = 'b7f3c5c1f29850348d978484a7a550c1d30401d3b27c172087de6cff67a74da52dce55296a1f5e00b0f45f3046e24667e334bab41f1635e6e2e16ad9975fd146'
