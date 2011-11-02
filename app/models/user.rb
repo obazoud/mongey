22674c94209cf170f8f1ffac1e668c726173f09d
Jonas Lundqvist <lundqvist.j@gmail.com>
2011-11-02 18:52:03 +0100

    removed some generated commas

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User
   include Mongoid::Document
-  field :username, :type => String,
-  field :email, :type => String,
-  field :admin, :type => Boolean,
-  field :password_hash, :type => String,
+
+  field :username, :type => String
+  field :email, :type => String
+  field :admin, :type => Boolean
+  field :password_hash, :type => String
   field :password_salt, :type => String
 end
