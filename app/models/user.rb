c66df6449d8456751debb67bfd50e32ef00e0d1e
Jonas Lundqvist <lundqvist.j@gmail.com>
2011-11-02 19:20:08 +0100

    Updated usermodel

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,35 @@
 class User
   include Mongoid::Document
+  include Mongoid::Timestamps
 
   field :username, :type => String
   field :email, :type => String
   field :admin, :type => Boolean
   field :password_hash, :type => String
   field :password_salt, :type => String
+
+  attr_accessible :username, :email, :password, :password_confirmation
+  attr_accessor :password
+  before_save :encrypt_password
+
+  validates_presence_of :username, :email
+  validates_presence_of :password, :on => :create
+  validates_confirmation_of :password
+  validates_uniqueness_of :username, :email
+
+  def self.authenticate(username, password)
+    user = where(username: username).first
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
 end
