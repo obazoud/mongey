fb74f3264ac87d0e55203ad9a0997b2918dd65f8
Jonas Lundqvist <lundqvist.j@gmail.com>
2011-11-06 22:31:39 +0100

    Refactored Deposits#create and Payments#create

--- app/controllers/deposits_controller.rb
+++ app/controllers/deposits_controller.rb
@@ -6,35 +6,29 @@ class DepositsController < ApplicationController
   def create
     @deposit = current_user.deposits.new(params[:deposit])
     
-    account = @deposit.account
-    payer = Payee.where(name: params[:deposit][:payer_name]).first
-    payer = Payee.create!(:name => params[:deposit][:payer_name], :opening_date => Time.now, :balance => 0.0) if payer.nil?
-    @deposit.payer = payer
+    @deposit.payer = payer = Payee.where(name: params[:deposit][:payer_name]).first ||
+                     current_user.payees.create!(:name => params[:deposit][:payer_name], :opening_date => Time.now, :balance => 0.0) if payer.nil?
 
-    credit_transaction = @deposit.transactions.new(:memo => @deposit[:memo], 
-                                                   :credit => @deposit[:amount].to_f, 
-                                                   :debit => 0, 
-                                                   :account_id => account.id,
-                                                   :category_id => @deposit[:category_id])
-    debit_transaction = @deposit.transactions.new(:memo => @deposit[:memo],
-                                                  :credit => 0,
-                                                  :debit => @deposit[:amount].to_f,
-                                                  :account_id => payer.id,
-                                                  :category_id => @deposit[:category_id])
+    @deposit.transactions.new(:memo => @deposit[:memo], 
+                              :credit => @deposit[:amount].to_f, 
+                              :debit => 0, 
+                              :account_id => @deposit.account.id,
+                              :category_id => @deposit[:category_id])
+    @deposit.transactions.new(:memo => @deposit[:memo],
+                              :credit => 0,
+                              :debit => @deposit[:amount].to_f,
+                              :account_id => @deposit.payer.id,
+                              :category_id => @deposit[:category_id])
 
-    payer.balance -= @deposit[:amount].to_f
-    account.balance += @deposit[:amount].to_f
+    @deposit.payer.balance -= @deposit[:amount].to_f
+    @deposit.account.balance += @deposit[:amount].to_f
 
     #Save the payment
-    @deposit.save
-    credit_transaction.save
-    debit_transaction.save
+    @deposit.save!
+    @deposit.transactions.each { |t| t.save! }
     
     #Save changes to accounts
-    payer.save
-    account.save
-
+    @deposit.payer.save
+    @deposit.account.save
   end
-
-
 end
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -14,34 +14,30 @@ class PaymentsController < ApplicationController
   def create
     @payment = current_user.payments.new(params[:payment])
     
-    account = @payment.account
+    @payment.payee = Payee.where(name: params[:payment][:payee_name]).first ||
+                     current_user.payees.create!(:name => params[:payment][:payee_name], :opening_date => Time.now, :balance => 0.0)
 
-    payee = Payee.where(name: params[:payment][:payee_name]).first
-    payee = current_user.payees.create!(:name => params[:payment][:payee_name], :opening_date => Time.now, :balance => 0.0) if payee.nil?
-    @payment.payee = payee
+    @payment.transactions.new(:memo => @payment[:memo], 
+                                  :credit => @payment[:amount].to_f, 
+                                  :debit => 0, 
+                                  :account_id => @payment.payee.id,
+                                  :category_id => @payment[:category_id])
+    @payment.transactions.new(:memo => @payment[:memo],
+                                  :credit => 0,
+                                  :debit => @payment[:amount].to_f,
+                                  :account_id => @payment.account.id,
+                                  :category_id => @payment[:category_id])
 
-    credit_transaction = @payment.transactions.new(:memo => @payment[:memo], 
-                                                   :credit => @payment[:amount].to_f, 
-                                                   :debit => 0, 
-                                                   :account_id => payee.id,
-                                                   :category_id => @payment[:category_id])
-    debit_transaction = @payment.transactions.new(:memo => @payment[:memo],
-                                                  :credit => 0,
-                                                  :debit => @payment[:amount].to_f,
-                                                  :account_id => account.id,
-                                                  :category_id => @payment[:category_id])
-
-    payee.balance += @payment[:amount].to_f
-    account.balance -= @payment[:amount].to_f
+    @payment.payee.balance += @payment[:amount].to_f
+    @payment.account.balance -= @payment[:amount].to_f
 
     #Save the payment
-    @payment.save
-    credit_transaction.save
-    debit_transaction.save
+    @payment.save!
+    @payment.transactions.each { |t| t.save! }
     
     #Save changes to accounts
-    payee.save
-    account.save
+    @payment.payee.save
+    @payment.account.save
 
   end
 
