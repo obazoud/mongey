a3aa3957be6144ec6b45a0b18e18f4317f19ca0b
Jonas Lundqvist <lundqvist.j@gmail.com>
2011-11-06 12:12:18 +0100

    small change to format_currency and added tests

--- app/helpers/currencies_helper.rb
+++ app/helpers/currencies_helper.rb
@@ -1,6 +1,6 @@
 module CurrenciesHelper
   def format_currency(value, currency)
-    return 0 if value.nil?
+    return "0.00" if value.nil?
     return currency.format(value) unless currency.nil?
     '%.2f' % value
   end
--- spec/helpers/currencies_helper_spec.rb
+++ spec/helpers/currencies_helper_spec.rb
@@ -10,6 +10,22 @@ require 'spec_helper'
 #     end
 #   end
 # end
+
 describe CurrenciesHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#format_currency" do
+    it 'formats a value with provided currency' do
+      value = 1234.56
+      currency = Factory.build(:complete_currency)
+      helper.format_currency(value, currency).should == currency.format(value)
+    end
+
+    it 'formats to 2 decimals if no currency is given' do
+      value = 1234.567
+      helper.format_currency(value, nil).should == "1234.57"
+    end
+
+    it 'return "0.00" if value is nil' do
+      helper.format_currency(nil, nil).should == "0.00"
+    end
+  end
 end
