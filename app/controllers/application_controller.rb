a5df2f2860566ffc2ee976c4165331b59d358540
Jonas Lundqvist <lundqvist.j@gmail.com>
2011-11-08 21:23:41 +0100

    cache current user (again) and removed helper include

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  helper CurrenciesHelper
-
   helper_method :current_user, :auth_required
 
   def auth_required
@@ -16,12 +14,12 @@ class ApplicationController < ActionController::Base
 private
 
   def current_user
-    User.find(session[:user_id]) if session[:user_id]
+    @_current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def auth_required
    if current_user
-     current_user
+     @_current_user
    else
      redirect_to login_path, :alert => "Login required!"
    end
