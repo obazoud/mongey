a2343b3f4d70c9428a1f9ff2b3d4b5add9c14295
Jonas Lundqvist <lundqvist.j@gmail.com>
2011-11-08 21:22:26 +0100

    helper to create path to account child

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,2 +1,9 @@
 module AccountsHelper
+  def child_account_path(account)
+    case account
+      when Bankaccount then bankaccount_path(account)
+      when Payee then payee_path(account)
+      else account_path(account)
+    end
+  end
 end
