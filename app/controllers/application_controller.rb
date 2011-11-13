a4ed56c216f405ee5a21e5c8f45e8214be627b19
Jonas Lundqvist <lundqvist.j@gmail.com>
2011-11-13 19:16:46 +0100

    removed dup func def and added a before_filter that creates empty event objects for the shortcut-forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,7 @@ class ApplicationController < ActionController::Base
   
   helper_method :current_user, :auth_required
 
-  def auth_required
-   if current_user
-     current_user
-   else
-     redirect_to login_path, :alert => "Login required!"
-   end
-  end
-
+  before_filter :create_dummy_transactions
 private
 
   def current_user
@@ -23,5 +16,12 @@ private
    else
      redirect_to login_path, :alert => "Login required!"
    end
+  end
+
+  def create_dummy_transactions
+    if current_user
+      @_payment = @_current_user.payments.new
+      @_deposit = @_current_user.deposits.new
+    end
   end
 end
