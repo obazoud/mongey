ff578fb72960b9a098d4da8cd77ef536567695c9
Jonas Lundqvist <lundqvist.j@gmail.com>
2012-01-18 17:43:49 +0100

    parent/children relation and parents scope

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,10 @@ class Category
   field :name, :type => String
   field :description, :type => String
 
+  belongs_to :parent, class_name: "Category", inverse_of: :children
+  has_many :children, class_name: "Category", inverse_of: :parent
+
+  scope :parents, where(parent_id: nil)
   validates_presence_of :name
  
   has_many :transactions
